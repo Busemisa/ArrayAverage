c5fb7e33b7b0d5bf1da884a08d9d97d32d9034d1
Busemisa <144456834+Busemisa@users.noreply.github.com>
2023-09-25 17:52:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+      int list[] = {1,2,3,4,5,6};
+      double sum =0.0;
+
+      for(int i=0;i<list.length;i++){
+          sum+=list[i];
+      }
+
+      double avarage = sum/list.length;
+      System.out.println(avarage);
+
+
+    }
+}
+
